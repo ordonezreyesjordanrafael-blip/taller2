3012cde1777227685ca2fee761341c340f420841
jordanrafael-blip <ordonezreyesjordanrafael@gmail.com>
2026-03-29 22:04:09 -0500

    entrega2

---
+++ src/main/java/ec/edu/espoch/mavenproject15/Mavenproject15.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.mavenproject15;
+
+/**
+ *
+ * @author JORDAN
+ */
+public class Mavenproject15 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
